a2a23f36147b9641f4182a5aeb66479e5defddee
Willyan <willyanmarquesmelo@gmail.com>
2025-03-13 13:58:06 -0300

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    
    Initial commit
    
    Changes to be committed:
    	new file:   READ-ME.md
    	new file:   baixa_diaria.py
    	new file:   baixa_diaria.spec
    	new file:   build/baixa_diaria/Analysis-00.toc
    	new file:   build/baixa_diaria/EXE-00.toc
    	new file:   build/baixa_diaria/PKG-00.toc
    	new file:   build/baixa_diaria/PYZ-00.pyz
    	new file:   build/baixa_diaria/PYZ-00.toc
    	new file:   build/baixa_diaria/baixa_diaria.pkg
    	new file:   build/baixa_diaria/base_library.zip
    	new file:   build/baixa_diaria/localpycs/pyimod01_archive.pyc
    	new file:   build/baixa_diaria/localpycs/pyimod02_importers.pyc
    	new file:   build/baixa_diaria/localpycs/pyimod03_ctypes.pyc
    	new file:   build/baixa_diaria/localpycs/pyimod04_pywin32.pyc
    	new file:   build/baixa_diaria/localpycs/struct.pyc
    	new file:   build/baixa_diaria/warn-baixa_diaria.txt
    	new file:   build/baixa_diaria/xref-baixa_diaria.html
    	new file:   build/preenchimento_diaria/Analysis-00.toc
    	new file:   build/preenchimento_diaria/EXE-00.toc
    	new file:   build/preenchimento_diaria/PKG-00.toc
    	new file:   build/preenchimento_diaria/PYZ-00.pyz
    	new file:   build/preenchimento_diaria/PYZ-00.toc
    	new file:   build/preenchimento_diaria/base_library.zip
    	new file:   build/preenchimento_diaria/localpycs/pyimod01_archive.pyc
    	new file:   build/preenchimento_diaria/localpycs/pyimod02_importers.pyc
    	new file:   build/preenchimento_diaria/localpycs/pyimod03_ctypes.pyc
    	new file:   build/preenchimento_diaria/localpycs/pyimod04_pywin32.pyc
    	new file:   build/preenchimento_diaria/localpycs/struct.pyc
    	new file:   build/preenchimento_diaria/preenchimento_diaria.pkg
    	new file:   build/preenchimento_diaria/warn-preenchimento_diaria.txt
    	new file:   build/preenchimento_diaria/xref-preenchimento_diaria.html
    	new file:   dist/baixa_diaria.exe
    	new file:   dist/preenchimento_diaria.exe
    	new file:   preenchimento_diaria.py
    	new file:   preenchimento_diaria.spec
    	new file:   requirements.txt

---
+++ preenchimento_diaria.py
@@ -0,0 +1,169 @@
+from datetime import datetime
+import sys
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import TimeoutException
+
+import pandas as pd
+
+from math import ceil
+import time
+import os
+
+ANO_ATUAL = datetime.now().year
+
+
+def carregar_planilha():
+    username = os.getlogin().strip()
+    caminho_nl_diaria = f"C:\\Users\\{username}\\OneDrive - Tribunal de Contas do Distrito Federal\\General - SECON\\FLUXO_DIÁRIAS\\TABELA_NL_DIARIAS_1.xlsx"
+    dataframe = pd.read_excel(caminho_nl_diaria)
+    return dataframe
+
+
+def setup_driver():
+    options = webdriver.ChromeOptions()
+    options.add_argument("--start-maximized")
+    options.add_experimental_option("detach", True)
+    driver = webdriver.Chrome(options=options)
+    return driver
+
+
+def login_siggo(driver):
+    url = "https://siggo.fazenda.df.gov.br/Account/Login"
+    driver.get(url)
+
+    cpf_input_selector = '//*[@id="cpf"]'
+    password_input_selector = '//*[@id="Password"]'
+    button_selector = "/html/body/div[2]/div/div/div[1]/div/div/div[2]/form/button"
+
+    cpf = ""
+    password = ""
+
+    driver.find_element(By.XPATH, value=cpf_input_selector).send_keys(cpf)
+    driver.find_element(By.XPATH, value=password_input_selector).send_keys(password)
+    driver.find_element(By.XPATH, button_selector).click()
+
+
+def esperar_login(driver, timeout=60):
+    url = "https://siggo.fazenda.df.gov.br/Account/Login"
+    driver.get(url)
+
+    start_time = time.time()
+
+    while True:
+        try:
+            title = driver.find_element(
+                By.XPATH, '//*[@id="SIAC"]/div/div/div/div[2]/div/a/h4'
+            )
+            if title.text == "AFC":
+                break
+        except:
+            pass
+
+        time.sleep(2)
+
+        if time.time() - start_time > timeout:
+            driver.quit()
+            raise TimeoutException("Tempo limite para login excedido.")
+
+
+def esperar_carregamento(driver):
+    while True:
+        try:
+            loading = driver.find_element(
+                By.XPATH, "/html/body/app-root/lib-login-callback/p"
+            )
+            if loading.text == "carregando...":
+                time.sleep(2)
+                continue
+        except:
+            break
+
+
+def preencher_cabecalho(driver, dados):
+    num_proc = str(dados.iloc[0, 6])
+    prioridade = "F0"
+    cod_ug = "020101-00001"
+    texto_obs = "CONCESSÃO DE DIÁRIAS A SERVIDORES P/ PARTICIPAÇÃO NO (nome_evento), PROMOVIDO PELO(A) (instituição), NO PERÍODO DE XXXX, EM (cidade)."
+
+    driver.find_element(By.XPATH, '//*[@id="nuProcesso"]/input').send_keys(
+        num_proc,
+    )
+    driver.find_element(By.XPATH, '//*[@id="prioridadePagamento"]/input').send_keys(
+        prioridade
+    )
+
+    Select(
+        driver.find_element(By.XPATH, '//*[@id="tipoCredor"]')
+    ).select_by_visible_text("4 - UG/Gestão")
+
+    driver.find_element(By.XPATH, '//*[@id="codigoCredor"]/input').send_keys(
+        cod_ug,
+    )
+
+    driver.find_element(By.XPATH, '//*[@id="observacao"]').send_keys(
+        texto_obs,
+    )
+
+
+def preencher_nl(driver, dados, aba):
+    delete_button = '//*[@id="ui-fieldset-0-content"]/div/div/div[1]/div/table/tbody/tr/td[7]/button'
+    driver.find_element(By.XPATH, delete_button).click()
+
+    lines = dados.shape[0]
+
+    for i in range(lines):
+        index = i + aba * 24
+
+        if i == 24 or index == lines:
+            break
+
+        driver.find_element(By.XPATH, '//*[@id="incluirCampoLancamentos"]').click()
+
+        for j in range(6):
+            if j == 2:
+                continue
+            selector = f'//*[@id="ui-fieldset-0-content"]/div/div/div[1]/div/table/tbody/tr[{i+1}]/td[{j+1}]/input'
+
+            dado = str(dados.iloc[index, j])
+            driver.find_element(By.XPATH, selector).send_keys(dado)
+
+
+def nova_aba(driver):
+    driver.execute_script("window.open('');")
+    abas = driver.window_handles
+    driver.switch_to.window(abas[-1])
+    driver.get(f"https://siggo.fazenda.df.gov.br/{ANO_ATUAL}/afc/nota-de-lancamento")
+    esperar_carregamento(driver)
+    time.sleep(2)  # tempo para carregar nova aba
+
+
+def fechar_primeira_aba(driver):
+    abas = driver.window_handles
+    driver.switch_to.window(abas[0])
+    driver.close()
+    driver.switch_to.window(abas[1])
+
+
+def main():
+    dataframe = carregar_planilha()
+    driver = setup_driver()
+    abas = int(ceil(dataframe.shape[0] / 24))
+
+    # login_siggo(driver)
+    esperar_login(driver)
+
+    for aba in range(abas):
+        nova_aba(driver)
+        preencher_cabecalho(driver, dataframe)
+        preencher_nl(driver, dataframe, aba)
+
+    fechar_primeira_aba(driver)
+
+
+try:
+    main()
+except Exception as e:
+    print(e)
+    sys.exit()
