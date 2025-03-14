e70229c098cb235e07055d2478d27cf9ab31d21d
Willyan <willyanmarquesmelo@gmail.com>
2025-03-14 13:03:52 -0300

    refatoracao e orientacao a objetos

--- baixa_diaria.py
+++ baixa_diaria.py
@@ -12,193 +12,198 @@ from datetime import datetime
 ANO_ATUAL = datetime.now().year
 
 
-def carregar_planilha():
-    username = os.getlogin().strip()
-    caminho_nl_diaria = f"C:\\Users\\{username}\\OneDrive - Tribunal de Contas do Distrito Federal\\General - SECON\\FLUXO_DIÁRIAS\\BAIXA_DIARIAS.xlsx"
-    dataframe = pd.read_excel(caminho_nl_diaria)
-    return dataframe
+class SiggoDriver:
+    caminho_planilha: str
 
+    def __init__(self, test=False):
+        self.carregar_planilha()
+        self.setup_driver()
+        self.esperar_login()
 
-def separar_processos(dataframe):
-    dataframes_separados = {
-        processo: df for processo, df in dataframe.groupby("PROCESSO")
-    }
-    return dataframes_separados
+    def carregar_planilha(self):
+        username = os.getlogin().strip()
 
+        caminho_completo = (
+            f"C:\\Users\\{username}\\OneDrive - Tribunal de Contas do Distrito Federal\\General - SECON\\"
+            + self.caminho_planilha
+        )
 
-def setup_driver():
-    options = webdriver.ChromeOptions()
-    options.add_argument("--start-maximized")
-    options.add_experimental_option("detach", True)
-    driver = webdriver.Chrome(options=options)
-    return driver
+        dataframe = pd.read_excel(caminho_completo)
+        self.planilha = dataframe
 
+    def setup_driver(self):
+        options = webdriver.ChromeOptions()
+        options.add_argument("--start-maximized")
+        options.add_experimental_option("detach", True)
 
-def login_siggo(driver):
-    url = "https://siggo.fazenda.df.gov.br/Account/Login"
-    driver.get(url)
+        self.driver = webdriver.Chrome(options=options)
 
-    cpf_input_selector = '//*[@id="cpf"]'
-    password_input_selector = '//*[@id="Password"]'
-    button_selector = "/html/body/div[2]/div/div/div[1]/div/div/div[2]/form/button"
+    # Métodos controle de login
+    def login_siggo(self, cpf, password):
+        url = "https://siggo.fazenda.df.gov.br/Account/Login"
+        self.driver.get(url)
 
-    cpf = "00350352267"
-    password = "ac1ja15"
+        cpf_input_selector = '//*[@id="cpf"]'
+        password_input_selector = '//*[@id="Password"]'
+        button_selector = "/html/body/div[2]/div/div/div[1]/div/div/div[2]/form/button"
 
-    driver.find_element(By.XPATH, value=cpf_input_selector).send_keys(cpf)
-    driver.find_element(By.XPATH, value=password_input_selector).send_keys(password)
-    driver.find_element(By.XPATH, button_selector).click()
+        self.driver.find_element(By.XPATH, value=cpf_input_selector).send_keys(
+            cpf,
+        )
+        self.driver.find_element(By.XPATH, value=password_input_selector).send_keys(
+            password,
+        )
+        self.driver.find_element(By.XPATH, button_selector).click()
 
+    def esperar_login(self, timeout=60):
+        url = "https://siggo.fazenda.df.gov.br/Account/Login"
+        self.driver.get(url)
 
-def esperar_login(driver, timeout=60):
-    url = "https://siggo.fazenda.df.gov.br/Account/Login"
-    driver.get(url)
+        start_time = time.time()
 
-    start_time = time.time()
+        while True:
+            try:
+                title = self.driver.find_element(
+                    By.XPATH, '//*[@id="SIAC"]/div/div/div/div[2]/div/a/h4'
+                )
+                if title.text == "AFC":
+                    break
+            except:
+                pass
 
-    while True:
-        try:
-            title = driver.find_element(
-                By.XPATH, '//*[@id="SIAC"]/div/div/div/div[2]/div/a/h4'
-            )
-            if title.text == "AFC":
+            time.sleep(1)
+            current_time = time.time()
+            if current_time - start_time > timeout:
+                self.driver.quit()  # TODO: unificar método para finalizar o SiggoDriver
+                raise TimeoutException("Tempo limite para login excedido.")
+
+    def esperar_carregamento(self, timeout=60):
+        start_time = time.time()
+
+        while True:
+            try:
+                loading = self.driver.find_element(
+                    By.XPATH, "/html/body/app-root/lib-login-callback/p"
+                )
+
+                current_time = time.time()
+                if current_time - start_time > timeout:
+                    self.driver.quit()
+                    raise TimeoutException("Tempo limite para login excedido.")
+
+                if loading.text == "carregando...":
+                    time.sleep(1)
+                    continue
+            except:
+                time.sleep(1)
                 break
-        except:
-            pass
 
-        time.sleep(2)
+    # Métodos preenchimento de campos
+    def preencher_campos(self, campos: dict):
+        for campo, dado in campos.items():
+            self.driver.find_element(By.XPATH, campo).send_keys(dado)
 
-        if time.time() - start_time > timeout:
-            driver.quit()
-            raise TimeoutException("Tempo limite para login excedido.")
-
-
-def esperar_carregamento(driver):
-    while True:
-        try:
-            loading = driver.find_element(
-                By.XPATH, "/html/body/app-root/lib-login-callback/p"
+    def selecionar_opcoes(self, opcoes: dict):
+        for campo, opcao in opcoes.items():
+            Select(self.driver.find_element(By.XPATH, campo)).select_by_visible_text(
+                opcao
             )
-            if loading.text == "carregando...":
-                time.sleep(2)
-                continue
-        except:
-            break
+
+    # Método controle de páginas
+    def nova_aba(self):
+        self.driver.execute_script("window.open('');")
+        abas = self.driver.window_handles
+        self.driver.switch_to.window(abas[-1])
+
+    def acessar_link(self, link):
+        self.driver.get(link)
+        self.esperar_carregamento()
+
+    def fechar_primeira_aba(self):
+        abas = self.driver.window_handles
+        self.driver.switch_to.window(abas[0])
+        self.driver.close()
+        self.driver.switch_to.window(abas[1])
+
+    def executar(self):
+        raise NotImplementedError("Método deve ser implementado na subclasse.")
 
 
-# TODO: generalizar as funções de preenchimento
-def preencher_cabecalho(driver, processo, dados):
-    # num_proc = str(dados.iloc[0, 6])
-    prioridade = "Z0"
-    cod_ug = "020101-00001"
-    # texto_obs = "CONCESSÃO DE DIÁRIAS A SERVIDORES P/ PARTICIPAÇÃO NO (nome_evento), PROMOVIDO PELO(A) (instituição), NO PERÍODO DE XXXX, EM (cidade)."
+class PreenchimetoBaixa(SiggoDriver):
+    def __init__(self, test=False):
+        self.caminho_planilha = "FLUXO_DIÁRIAS\\BAIXA_DIARIAS.xlsx"
+        super().__init__(test)
 
-    driver.find_element(By.XPATH, '//*[@id="nuProcesso"]/input').send_keys(
-        processo,
-    )
-    driver.find_element(By.XPATH, '//*[@id="prioridadePagamento"]/input').send_keys(
-        prioridade
-    )
+    def separar_processos(self, dataframe):
+        dataframes_separados = {
+            processo: df for processo, df in dataframe.groupby("PROCESSO")
+        }
+        return dataframes_separados
 
-    Select(
-        driver.find_element(By.XPATH, '//*[@id="tipoCredor"]')
-    ).select_by_visible_text("4 - UG/Gestão")
+    def preencher_baixa(self, dados):
+        self.preencher_cabecalho(dados)
+        self.preencher_lancamentos(dados)
 
-    driver.find_element(By.XPATH, '//*[@id="codigoCredor"]/input').send_keys(
-        cod_ug,
-    )
+    def preencher_lancamentos(self, dados):
+        delete_button = '//*[@id="ui-fieldset-0-content"]/div/div/div[1]/div/table/tbody/tr/td[7]/button'
+        self.driver.find_element(By.XPATH, delete_button).click()
 
-    # driver.find_element(By.XPATH, '//*[@id="observacao"]').send_keys(
-    #     texto_obs,
-    # )
+        linhas = dados.shape[0]
+        campos = {}
+        for i in range(linhas):
+            self.driver.find_element(
+                By.XPATH, '//*[@id="incluirCampoLancamentos"]'
+            ).click()
 
+            evento = 560379  # coluna 1
+            inscricao = dados.iloc[i]["CPF"]  # coluna 2
+            classificacao_contabil = 332110100  # coluna 3
+            valor = dados.iloc[i]["BAIXAR"]  # coluna 6
 
-def preencher_nl(driver, dados, num_aba):
-    delete_button = '//*[@id="ui-fieldset-0-content"]/div/div/div[1]/div/table/tbody/tr/td[7]/button'
-    driver.find_element(By.XPATH, delete_button).click()
+            valores = [evento, inscricao, classificacao_contabil, None, None, valor]
 
-    lines = dados.shape[0]
+            for j in range(6):
+                if valores[j] is None:
+                    continue
 
-    for i in range(lines):
-        index = i + num_aba * 24
+                seletor = f'//*[@id="ui-fieldset-0-content"]/div/div/div[1]/div/table/tbody/tr[{i+1}]/td[{j+1}]/input'
+                campos[seletor] = str(valores[j])
 
-        if i == 24 or index == lines:
-            break
+        self.preencher_campos(campos)
 
-        driver.find_element(By.XPATH, '//*[@id="incluirCampoLancamentos"]').click()
+    def preencher_cabecalho(self, dados):
+        processo = dados.iloc[0]["PROCESSO"]
+        prioridade = "Z0"
+        cod_ug = "020101-00001"
 
-        for j in range(6):
-            if j == 2:
-                continue
-            selector = f'//*[@id="ui-fieldset-0-content"]/div/div/div[1]/div/table/tbody/tr[{i+1}]/td[{j+1}]/input'
+        campos = {
+            '//*[@id="nuProcesso"]/input': processo,
+            '//*[@id="prioridadePagamento"]/input': prioridade,
+            '//*[@id="codigoCredor"]/input': cod_ug,
+        }
+        opcoes = {
+            '//*[@id="tipoCredor"]': "4 - UG/Gestão",
+        }
 
-            dado = str(dados.iloc[index, j])
-            driver.find_element(By.XPATH, selector).send_keys(dado)
+        self.selecionar_opcoes(opcoes)
+        self.preencher_campos(campos)
 
+    def executar(self):
 
-def preencher_baixa(driver, dados):
-    delete_button = '//*[@id="ui-fieldset-0-content"]/div/div/div[1]/div/table/tbody/tr/td[7]/button'
-    driver.find_element(By.XPATH, delete_button).click()
+        dados_por_processo = self.separar_processos(self.planilha)
 
-    lines = dados.shape[0]
+        link = f"https://siggo.fazenda.df.gov.br/{ANO_ATUAL}/afc/nota-de-lancamento"
+        for processo, dados in dados_por_processo.items():
+            self.nova_aba()
+            self.acessar_link(link)
+            self.preencher_baixa(dados)
 
-    for i in range(lines):
-        driver.find_element(By.XPATH, '//*[@id="incluirCampoLancamentos"]').click()
-
-        evento = 560379  # coluna 1
-        inscricao = dados.iloc[i]["CPF"]  # coluna 2
-        classificacao_contabil = 332110100  # coluna 3
-        valor = dados.iloc[i]["BAIXAR"]  # coluna 6
-
-        valores = [evento, inscricao, classificacao_contabil, None, None, valor]
-
-        for j in range(6):
-            if valores[j] is None:
-                continue
-
-            selector = f'//*[@id="ui-fieldset-0-content"]/div/div/div[1]/div/table/tbody/tr[{i+1}]/td[{j+1}]/input'
-
-            driver.find_element(By.XPATH, selector).send_keys(str(valores[j]))
-
-
-def nova_aba(driver):
-    driver.execute_script("window.open('');")
-    abas = driver.window_handles
-    driver.switch_to.window(abas[-1])
-
-
-def acessar_link(driver, link):
-    driver.get(link)
-    esperar_carregamento(driver)
-    time.sleep(2)
-
-
-def fechar_primeira_aba(driver):
-    abas = driver.window_handles
-    driver.switch_to.window(abas[0])
-    driver.close()
-    driver.switch_to.window(abas[1])
-
-
-def run():
-    dataframe = carregar_planilha()
-    dataframes_por_processo = separar_processos(dataframe)
-
-    driver = setup_driver()
-
-    # login_siggo(driver)
-    esperar_login(driver)
-    link = f"https://siggo.fazenda.df.gov.br/{ANO_ATUAL}/afc/nota-de-lancamento"
-    for processo, dado in dataframes_por_processo.items():
-        nova_aba(driver)
-        acessar_link(driver, link)
-        preencher_cabecalho(driver, processo, dado)
-        preencher_baixa(driver, dado)
-
-    fechar_primeira_aba(driver)
+        self.fechar_primeira_aba()
 
 
 try:
-    run()
-except:
+    driver = PreenchimetoBaixa()
+    driver.executar()
+except Exception as e:
+    print(e)
     sys.exit()
--- preenchimento_diaria.py
+++ preenchimento_diaria.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from math import ceil
 import sys
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -6,164 +6,201 @@ from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import TimeoutException
 
 import pandas as pd
-
-from math import ceil
 import time
 import os
+from datetime import datetime
 
 ANO_ATUAL = datetime.now().year
 
 
-def carregar_planilha():
-    username = os.getlogin().strip()
-    caminho_nl_diaria = f"C:\\Users\\{username}\\OneDrive - Tribunal de Contas do Distrito Federal\\General - SECON\\FLUXO_DIÁRIAS\\TABELA_NL_DIARIAS_1.xlsx"
-    dataframe = pd.read_excel(caminho_nl_diaria)
-    return dataframe
+class SiggoDriver:
+    caminho_planilha: str
 
+    def __init__(self, test=False):
+        self.carregar_planilha()
+        self.setup_driver()
+        self.esperar_login()
 
-def setup_driver():
-    options = webdriver.ChromeOptions()
-    options.add_argument("--start-maximized")
-    options.add_experimental_option("detach", True)
-    driver = webdriver.Chrome(options=options)
-    return driver
+    def carregar_planilha(self):
+        username = os.getlogin().strip()
 
+        caminho_completo = (
+            f"C:\\Users\\{username}\\OneDrive - Tribunal de Contas do Distrito Federal\\General - SECON\\"
+            + self.caminho_planilha
+        )
 
-def login_siggo(driver):
-    url = "https://siggo.fazenda.df.gov.br/Account/Login"
-    driver.get(url)
+        dataframe = pd.read_excel(caminho_completo)
+        self.planilha = dataframe
 
-    cpf_input_selector = '//*[@id="cpf"]'
-    password_input_selector = '//*[@id="Password"]'
-    button_selector = "/html/body/div[2]/div/div/div[1]/div/div/div[2]/form/button"
+    def setup_driver(self):
+        options = webdriver.ChromeOptions()
+        options.add_argument("--start-maximized")
+        options.add_experimental_option("detach", True)
 
-    cpf = ""
-    password = ""
+        self.driver = webdriver.Chrome(options=options)
 
-    driver.find_element(By.XPATH, value=cpf_input_selector).send_keys(cpf)
-    driver.find_element(By.XPATH, value=password_input_selector).send_keys(password)
-    driver.find_element(By.XPATH, button_selector).click()
+    def finalizar(self):
+        self.driver.quit()
 
+    # Métodos controle de login
+    def login_siggo(self, cpf, password):
+        url = "https://siggo.fazenda.df.gov.br/Account/Login"
+        self.driver.get(url)
 
-def esperar_login(driver, timeout=60):
-    url = "https://siggo.fazenda.df.gov.br/Account/Login"
-    driver.get(url)
+        cpf_input_selector = '//*[@id="cpf"]'
+        password_input_selector = '//*[@id="Password"]'
+        button_selector = "/html/body/div[2]/div/div/div[1]/div/div/div[2]/form/button"
 
-    start_time = time.time()
+        self.driver.find_element(By.XPATH, value=cpf_input_selector).send_keys(
+            cpf,
+        )
+        self.driver.find_element(By.XPATH, value=password_input_selector).send_keys(
+            password,
+        )
+        self.driver.find_element(By.XPATH, button_selector).click()
 
-    while True:
-        try:
-            title = driver.find_element(
-                By.XPATH, '//*[@id="SIAC"]/div/div/div/div[2]/div/a/h4'
-            )
-            if title.text == "AFC":
+    def esperar_login(self, timeout=60):
+        url = "https://siggo.fazenda.df.gov.br/Account/Login"
+        self.driver.get(url)
+
+        start_time = time.time()
+
+        while True:
+            try:
+                title = self.driver.find_element(
+                    By.XPATH, '//*[@id="SIAC"]/div/div/div/div[2]/div/a/h4'
+                )
+                if title.text == "AFC":
+                    break
+            except:
+                pass
+
+            time.sleep(1)
+            current_time = time.time()
+            if current_time - start_time > timeout:
+                self.driver.quit()  # TODO: unificar método para finalizar o SiggoDriver
+                raise TimeoutException("Tempo limite para login excedido.")
+
+    def esperar_carregamento(self, timeout=60):
+        start_time = time.time()
+
+        while True:
+            try:
+                loading = self.driver.find_element(
+                    By.XPATH, "/html/body/app-root/lib-login-callback/p"
+                )
+
+                current_time = time.time()
+                if current_time - start_time > timeout:
+                    self.driver.quit()
+                    raise TimeoutException("Tempo limite para login excedido.")
+
+                if loading.text == "carregando...":
+                    time.sleep(1)
+                    continue
+            except:
+                time.sleep(1)
                 break
-        except:
-            pass
 
-        time.sleep(2)
+    # Métodos preenchimento de campos
+    def preencher_campos(self, campos: dict):
+        for campo, dado in campos.items():
+            self.driver.find_element(By.XPATH, campo).send_keys(dado)
 
-        if time.time() - start_time > timeout:
-            driver.quit()
-            raise TimeoutException("Tempo limite para login excedido.")
-
-
-def esperar_carregamento(driver):
-    while True:
-        try:
-            loading = driver.find_element(
-                By.XPATH, "/html/body/app-root/lib-login-callback/p"
+    def selecionar_opcoes(self, opcoes: dict):
+        for campo, opcao in opcoes.items():
+            Select(self.driver.find_element(By.XPATH, campo)).select_by_visible_text(
+                opcao
             )
-            if loading.text == "carregando...":
-                time.sleep(2)
-                continue
-        except:
-            break
+
+    # Método controle de páginas
+    def nova_aba(self):
+        self.driver.execute_script("window.open('');")
+        abas = self.driver.window_handles
+        self.driver.switch_to.window(abas[-1])
+
+    def acessar_link(self, link):
+        self.driver.get(link)
+        self.esperar_carregamento()
+
+    def fechar_primeira_aba(self):
+        abas = self.driver.window_handles
+        self.driver.switch_to.window(abas[0])
+        self.driver.close()
+        self.driver.switch_to.window(abas[1])
+
+    def executar(self):
+        raise NotImplementedError("Método deve ser implementado na subclasse.")
 
 
-def preencher_cabecalho(driver, dados):
-    num_proc = str(dados.iloc[0, 6])
-    prioridade = "F0"
-    cod_ug = "020101-00001"
-    texto_obs = "CONCESSÃO DE DIÁRIAS A SERVIDORES P/ PARTICIPAÇÃO NO (nome_evento), PROMOVIDO PELO(A) (instituição), NO PERÍODO DE XXXX, EM (cidade)."
+class PreenchimentoDiaria(SiggoDriver):
+    def __init__(self, test=False):
+        self.caminho_planilha = "FLUXO_DIÁRIAS\\TABELA_NL_DIARIAS_1.xlsx"
+        super().__init__(test)
 
-    driver.find_element(By.XPATH, '//*[@id="nuProcesso"]/input').send_keys(
-        num_proc,
-    )
-    driver.find_element(By.XPATH, '//*[@id="prioridadePagamento"]/input').send_keys(
-        prioridade
-    )
+    def separar_por_pagina(self, dataframe, tamanho_pagina=24):
+        return [
+            dataframe.iloc[i : i + tamanho_pagina]
+            for i in range(0, len(dataframe), tamanho_pagina)
+        ]
 
-    Select(
-        driver.find_element(By.XPATH, '//*[@id="tipoCredor"]')
-    ).select_by_visible_text("4 - UG/Gestão")
+    def preencher_diaria(self, dados):
+        self.preencher_cabecalho(dados)
+        self.preencher_lancamentos(dados)
 
-    driver.find_element(By.XPATH, '//*[@id="codigoCredor"]/input').send_keys(
-        cod_ug,
-    )
+    def preencher_cabecalho(self, dados):
+        num_proc = str(dados.iloc[0, 6])
+        prioridade = "F0"
+        cod_ug = "020101-00001"
+        texto_obs = """CONCESSÃO DE DIÁRIAS A SERVIDORES P/ PARTICIPAÇÃO NO (nome_evento), PROMOVIDO PELO(A) (instituição), NO PERÍODO DE XXXX, EM (cidade)."""
 
-    driver.find_element(By.XPATH, '//*[@id="observacao"]').send_keys(
-        texto_obs,
-    )
+        opcoes = {'//*[@id="tipoCredor"]': "4 - UG/Gestão"}
+        campos = {
+            '//*[@id="nuProcesso"]/input': num_proc,
+            '//*[@id="prioridadePagamento"]/input': prioridade,
+            '//*[@id="codigoCredor"]/input': cod_ug,
+            '//*[@id="observacao"]': texto_obs,
+        }
 
+        self.selecionar_opcoes(opcoes)
+        self.preencher_campos(campos)
 
-def preencher_nl(driver, dados, aba):
-    delete_button = '//*[@id="ui-fieldset-0-content"]/div/div/div[1]/div/table/tbody/tr/td[7]/button'
-    driver.find_element(By.XPATH, delete_button).click()
+    def preencher_lancamentos(self, dados):
+        delete_button = '//*[@id="ui-fieldset-0-content"]/div/div/div[1]/div/table/tbody/tr/td[7]/button'
+        self.driver.find_element(By.XPATH, delete_button).click()
 
-    lines = dados.shape[0]
+        lines = dados.shape[0]
+        for i in range(lines):
+            self.driver.find_element(
+                By.XPATH, '//*[@id="incluirCampoLancamentos"]'
+            ).click()
 
-    for i in range(lines):
-        index = i + aba * 24
+            campos = {}
+            for j in range(6):
+                if j == 2:
+                    continue
 
-        if i == 24 or index == lines:
-            break
+                seletor = f'//*[@id="ui-fieldset-0-content"]/div/div/div[1]/div/table/tbody/tr[{i+1}]/td[{j+1}]/input'
+                dado = str(dados.iloc[i, j])
 
-        driver.find_element(By.XPATH, '//*[@id="incluirCampoLancamentos"]').click()
+                campos[seletor] = dado
 
-        for j in range(6):
-            if j == 2:
-                continue
-            selector = f'//*[@id="ui-fieldset-0-content"]/div/div/div[1]/div/table/tbody/tr[{i+1}]/td[{j+1}]/input'
+            self.preencher_campos(campos)
 
-            dado = str(dados.iloc[index, j])
-            driver.find_element(By.XPATH, selector).send_keys(dado)
+    def executar(self):
+        dados_por_pagina = self.separar_por_pagina(self.planilha)
+        link = f"https://siggo.fazenda.df.gov.br/{ANO_ATUAL}/afc/nota-de-lancamento"
+        for dados in dados_por_pagina:
+            self.nova_aba()
+            self.acessar_link(link)
+            self.preencher_diaria(dados)
 
-
-def nova_aba(driver):
-    driver.execute_script("window.open('');")
-    abas = driver.window_handles
-    driver.switch_to.window(abas[-1])
-    driver.get(f"https://siggo.fazenda.df.gov.br/{ANO_ATUAL}/afc/nota-de-lancamento")
-    esperar_carregamento(driver)
-    time.sleep(2)  # tempo para carregar nova aba
-
-
-def fechar_primeira_aba(driver):
-    abas = driver.window_handles
-    driver.switch_to.window(abas[0])
-    driver.close()
-    driver.switch_to.window(abas[1])
-
-
-def main():
-    dataframe = carregar_planilha()
-    driver = setup_driver()
-    abas = int(ceil(dataframe.shape[0] / 24))
-
-    # login_siggo(driver)
-    esperar_login(driver)
-
-    for aba in range(abas):
-        nova_aba(driver)
-        preencher_cabecalho(driver, dataframe)
-        preencher_nl(driver, dataframe, aba)
-
-    fechar_primeira_aba(driver)
+        self.fechar_primeira_aba()
 
 
 try:
-    main()
+    driver = PreenchimentoDiaria()
+    driver.executar()
 except Exception as e:
     print(e)
     sys.exit()
