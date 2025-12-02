2acb9e93ceb87c4554a4e24f168b7d34bf00f446
Willyan <willyanmarquesmelo@gmail.com>
2025-12-02 18:06:21 -0300

    feature: preparar_preenchimento_cabecalho agora tem "contrato"

--- src/adapters/controllers/emails_driss_controller.py
+++ src/adapters/controllers/emails_driss_controller.py
@@ -1,10 +1,15 @@
+from src.infrastructure.cli.console_service import ConsoleService
 from src.factories import UseCaseFactory
 
 
 def EmailsDrissController(run=True, test=False):
+    app_view = ConsoleService()
+
     factory = UseCaseFactory()
     use_case = factory.create_emails_driss_usecase()
     use_case.executar()
+
+    app_view.show_message("Processamento concluído.")
 
 
 if __name__ == "__main__":
